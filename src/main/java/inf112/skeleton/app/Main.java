8d5afdbf7966ad4760f750d4e95a66cec783950b
Egor.Bostrov <egbos8921@uib.no>
2024-02-29 12:06:24 +0100

    Main runs with a window (it is blank)

---
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -0,0 +1,16 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class Main {
+    public static void main (String[] arg) {
+        Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+        cfg.setForegroundFPS(60);
+        cfg.setTitle("TowerDefence");
+        cfg.setWindowedMode(800, 600);
+
+        new Lwjgl3Application(new OTDGame(), cfg);
+    }
+}
+
--- src/main/java/inf112/skeleton/app/OTDGame.java
+++ src/main/java/inf112/skeleton/app/OTDGame.java
@@ -1,4 +1,35 @@
 package inf112.skeleton.app;
 
-public class OTDGame {
+import com.badlogic.gdx.ApplicationListener;
+
+public class OTDGame implements ApplicationListener {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
