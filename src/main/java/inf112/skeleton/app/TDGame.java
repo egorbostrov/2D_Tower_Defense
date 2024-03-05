9501ff1a1cd07bae87697091533e50757476ca92
Trym.Jakobsen <trym.jakobsen@student.uib.no>
2024-03-05 10:21:37 +0100

    Clean screen inbetween each render

--- src/main/java/inf112/skeleton/app/TDGame.java
+++ src/main/java/inf112/skeleton/app/TDGame.java
@@ -2,9 +2,11 @@ package inf112.skeleton.app;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import inf112.skeleton.app.resourceHandler.MyAtlas;
+import inf112.skeleton.app.scene.Scene;
 import inf112.skeleton.app.scene.Scene.SceneEnum;
 import inf112.skeleton.app.scene.SceneController;
 
@@ -25,6 +27,8 @@ public class TDGame extends ApplicationAdapter {
 
     @Override
     public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1); //Clear screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //Clear buffer and screen
         sceneController.render(batch,render);
         sceneController.update(Gdx.graphics.getDeltaTime());
     }
