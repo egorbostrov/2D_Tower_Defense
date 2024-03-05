9e20d914518f626aaee71d8719eec3e15f0b6211
Trym.Jakobsen <trym.jakobsen@student.uib.no>
2024-03-05 10:30:52 +0100

    Removed redundant methods

--- src/main/java/inf112/skeleton/app/map/Map.java
+++ src/main/java/inf112/skeleton/app/map/Map.java
@@ -15,21 +15,6 @@ import inf112.skeleton.app.map.Board;
 
 public class Map {
 
-    public Board getBoard() {
-        return null;
-    }
-
-    public Tile getSelectedTile(float x, float y) {
-        return null;
-    }
-
-    public void update(float elapsedTime) {
-    }
-
-    public void render(SpriteBatch batch) {
-    }
-    public void render(ShapeRenderer renderer) {
-    }
     private final LinkedList<MapMaker.Direction> directions;
     private final Board board;
     public Map() {
