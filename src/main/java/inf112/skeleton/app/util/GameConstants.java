8f0600fcaa8ea1073388f499a266b7d439a931e6
Trym.Jakobsen <trym.jakobsen@student.uib.no>
2024-03-15 14:09:39 +0100

    Updated constants to allow multiple zombie types

--- src/main/java/inf112/skeleton/app/util/GameConstants.java
+++ src/main/java/inf112/skeleton/app/util/GameConstants.java
@@ -21,7 +21,19 @@ public class GameConstants {
 
     public static float ENEMY_WIDTH = TILE_WIDTH / (float)1.2;
     public static float ENEMY_HEIGHT = TILE_HEIGHT / (float)1.2;
-    public static int ENEMY_SPEED = (int) TILE_WIDTH;
+
+    //REGULAR ZOMBIE VALUES:
+    public static int EMEMY_REGULAR_SPEED = (int) TILE_WIDTH;
+    public static float ENEMY_REGULAR_START_HP = 10f;
+    public static int ENEMY_REGULAR_BOUNTY = 100;
+    public static float ENEMY_REGULAR_SPAWN_DELAY = 1.5f;
+
+    //TANK ZOMBIE VALUES:
+    public static int ENEMY_TANK_SPEED = (int) TILE_WIDTH/2;
+    public static float ENEMY_TANK_START_HP = 20f;
+    public static int ENEMY_TANK_BOUNTY = 150;
+    public static float ENEMY_TANK_SPAWN_DELAY = 3f;
+
     public static int START_MONEY = 1000;
     public static int REMAINING_HEALTH = 6;
     public static int SCORE_INCREASE = 100;
