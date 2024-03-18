b352712aba3e5d455779a8d9ffc3d6a1af4291a5
Trym.Jakobsen <trym.jakobsen@student.uib.no>
2024-03-18 13:54:11 +0100

    Made so that we can have random spawns instead of wave based spawns

---
+++ src/main/java/inf112/skeleton/app/controller/RandomEnemyFactory.java
@@ -0,0 +1,11 @@
+package inf112.skeleton.app.controller;
+
+import inf112.skeleton.app.entity.Enemy;
+import inf112.skeleton.app.level.Level;
+
+public class RandomEnemyFactory implements EnemyFactory{
+    @Override
+    public Enemy getNext(Level level) {
+        return null;
+    }
+}
