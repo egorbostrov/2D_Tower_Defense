abdb08fb9237889956010b12bc05a413dc4a6123
Trym.Jakobsen <trym.jakobsen@student.uib.no>
2024-04-12 07:06:47 +0200

    Strip each wave string for newLines, allowing the forloop in newWave to run at a logical length. Removed redundant import, unused code and print statements.

--- src/main/java/inf112/skeleton/app/controller/WaveController.java
+++ src/main/java/inf112/skeleton/app/controller/WaveController.java
@@ -2,11 +2,9 @@ package inf112.skeleton.app.controller;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.math.Interpolation;
 import inf112.skeleton.app.level.Level;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class WaveController {
@@ -23,27 +21,22 @@ public class WaveController {
         this.spawnDelay = 5;
 
         this.wavePatterns = readWavePatternsFromFile("WavePatterns.txt");
-
-//        this.enemyFactory = new WaveEnemyFactory("S");
-//        this.wavePattern = null;
     }
 
     private List<String> readWavePatternsFromFile(String fileName) {
         FileHandle fileHandle = Gdx.files.internal(fileName);
         String fileContent = fileHandle.readString();
         String[] lines = fileContent.split("\n");
-        return new ArrayList<>(Arrays.asList(lines));
+        List<String> cleanLines = new ArrayList<>();
+
+        for(String line : lines) {
+            cleanLines.add(line.trim());
+        }
+        return cleanLines;
     }
 
     public void newWave(Level level) {
-        System.out.println("Index:  " + waveIndex);
-        System.out.println("size:   " + wavePatterns.size());
-        /*if(waveIndex >= wavePatterns.size()) {
-            waveIndex = 0; //Loop to first wave if we ran out of waves
-        }*/
-        waveIndex = waveIndex % wavePatterns.size();//Loop to first wave if we ran out of waves
-
-        System.out.println("New index:  " + waveIndex);
+        waveIndex = waveIndex % wavePatterns.size();//Loop to first wave if we ran out of waves, using modulo
 
         String wavePattern = wavePatterns.get(waveIndex);
         this.enemyFactory = new WaveEnemyFactory(wavePattern);
@@ -51,16 +44,11 @@ public class WaveController {
         this.zombieIndex = 1f;
         spawnDelay *= 0.75f;
 
-        for(int i = 0; i < wavePattern.length() - 1; i++) {
-            //System.out.println(wavePattern.length());
+        for(int i = 0; i < wavePattern.length(); i++) {
             enemyController.newZombie(enemyFactory.getNext(level, zombieIndex * spawnDelay));
             zombieIndex++;
         }
         waveIndex++;
         zombieIndex = 0;
     }
-
-/*    public EnemyController getEnemyController() {
-        return enemyController;
-    }*/
 }
