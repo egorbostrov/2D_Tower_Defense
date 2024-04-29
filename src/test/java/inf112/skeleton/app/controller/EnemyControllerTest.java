abb2d788d28d1bee86499cbc87bbdfe4cf97e3d0
Trym.Jakobsen <trym.jakobsen@student.uib.no>
2024-04-29 19:07:13 +0200

    Did basic implementation to get the test environment running properly.

--- src/test/java/inf112/skeleton/app/controller/EnemyControllerTest.java
+++ src/test/java/inf112/skeleton/app/controller/EnemyControllerTest.java
@@ -1,5 +1,13 @@
 package inf112.skeleton.app.controller;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import inf112.skeleton.app.controller.EnemyController;
 import inf112.skeleton.app.entity.Enemy;
 import inf112.skeleton.app.level.Level;
@@ -7,9 +15,12 @@ import inf112.skeleton.app.map.Map;
 
 import inf112.skeleton.app.util.GameAssets;
 import inf112.skeleton.app.util.GameConstants;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.LinkedList;
@@ -22,19 +33,39 @@ public class EnemyControllerTest {
     private Level mockLevel;
     @Mock
     private Map mockMap;
-
+    @Mock
+    private SpriteBatch mockBatch;
+    @Mock
+    private ShapeRenderer mockRenderer;
+    @Mock
+    private Enemy mockEnemy;
     private EnemyController enemyController;
+    private static HeadlessApplication application;
+
+    @BeforeAll
+    public static void setupBeforeAll() {
+        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+        application = new HeadlessApplication(new ApplicationAdapter() {}, config);
+        Gdx.gl20 = Mockito.mock(GL20.class);
+        Gdx.gl = Gdx.gl20;
+        when(Gdx.gl.glGenTexture()).thenReturn(1);
+    }
+
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
 
-        // Setup mocks for Level and Map
-        when(mockLevel.getMap()).thenReturn(mockMap);
-        // Assuming your Map class has a getDirections method which returns a LinkedList<Direction>
-        when(mockMap.getDirections()).thenReturn(new LinkedList<>()); // return an empty list or a list with some directions
+        //Mocking needed for the healthbar created in Enemy constructor
+        Gdx.gl = mock(GL20.class);
+        Gdx.gl20 = Gdx.gl;
+        when(Gdx.gl.glGenTexture()).thenReturn(1);
 
-        // Now create an instance of EnemyController that will use the mocked Level
+
+        //doNothing().when(mockEnemy).initializeHealthBar(anyFloat(), anyFloat(), anyFloat(), anyFloat(), anyFloat());
+
+        when(mockEnemy.isAlive()).thenReturn(true);
+
         enemyController = new EnemyController(mockLevel);
     }
 
@@ -57,6 +88,16 @@ public class EnemyControllerTest {
             ));
         }
         assertEquals(5, enemyController.getEnemyList().size(), "List should contain 5 enemies");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        if(application != null) {
+            application.exit();
+            application = null;
+        }
+        Gdx.gl = null;
+        Gdx.gl20 = null;
     }
 }
 
