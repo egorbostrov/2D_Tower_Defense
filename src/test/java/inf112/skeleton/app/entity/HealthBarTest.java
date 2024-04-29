7caba986629401401d9a38f9bb9d4173d6ecf0fc
Tarjei Flotre <tarjei.flotre@gmail.com>
2024-04-29 13:27:56 +0200

    addedn mock to HealthBarTest

--- src/test/java/inf112/skeleton/app/entity/HealthBarTest.java
+++ src/test/java/inf112/skeleton/app/entity/HealthBarTest.java
@@ -1,15 +1,19 @@
 package inf112.skeleton.app.entity;
 
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
 import inf112.skeleton.app.util.GameConstants;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.util.LinkedList;
 import java.util.Random;
@@ -23,36 +27,25 @@ public class HealthBarTest {
 
     private static HeadlessApplication application;
 
-
     @BeforeAll
-    public static void setUp() {
+    public static void setupBeforeAll() {
         HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
-        application = new HeadlessApplication(new ApplicationListener() {
-            @Override
-            public void create() { }
-            @Override
-            public void resize(int width, int height) { }
-            @Override
-            public void render() { }
-            @Override
-            public void pause() { }
-            @Override
-            public void resume() { }
-            @Override
-            public void dispose() { }
-        }, config);
-        // Make sure LibGDX runs in headless mode
-        Gdx.gl = mock(com.badlogic.gdx.graphics.GL20.class);
-        Gdx.gl20 = Gdx.gl;
+        ApplicationListener listener = new ApplicationAdapter() {
+        };
+        new HeadlessApplication(listener, config);
+        Gdx.gl20 = Mockito.mock(GL20.class);
     }
+
+    @Mock
+    private Enemy mockEnemy;
 
     @Test
     public void testHealthRemoveEnemyVsHealthBar(){
-        Enemy enemy = new Enemy('R',100, 100, GameConstants.ENEMY_WIDTH, GameConstants.ENEMY_HEIGHT, 100, new LinkedList<>(), 0, 0, 0, null, false);
+        mockEnemy = new Enemy('R',100, 100, GameConstants.ENEMY_WIDTH, GameConstants.ENEMY_HEIGHT, 100, new LinkedList<>(), 0, 0, 0, null, false);
         Random random = new Random();
         int randomNumber = random.nextInt(101);
-        enemy.shot(randomNumber);
-        assertEquals(enemy.getHpBar().getCurrentHealth(), enemy.getEnemyHealth() - randomNumber);
+        mockEnemy.shot(randomNumber);
+        assertEquals(mockEnemy.getHpBar().getCurrentHealth(), mockEnemy.getEnemyHealth() - randomNumber);
     }
 
     @AfterAll
