5f418cc72887fa34d227fe282939a27aeef6a073
Tarjei Flotre <tarjei.flotre@gmail.com>
2024-05-02 18:22:23 +0200

    unused imports

--- src/test/java/inf112/skeleton/app/entity/HealthBarTest.java
+++ src/test/java/inf112/skeleton/app/entity/HealthBarTest.java
@@ -1,19 +1,20 @@
 package inf112.skeleton.app.entity;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import inf112.skeleton.app.util.GameConstants;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.util.LinkedList;
 import java.util.Random;
@@ -39,15 +40,25 @@ public class HealthBarTest {
     @Mock
     private Enemy mockEnemy;
 
+    @Mock
+    private SpriteBatch mockBatch;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+        mockEnemy = new Enemy('R',100, 100, GameConstants.ENEMY_WIDTH, GameConstants.ENEMY_HEIGHT, 100, new LinkedList<>(), 0, 0, 0, null, false);
+    }
+
     @Test
     public void testHealthRemoveEnemyVsHealthBar(){
-        mockEnemy = new Enemy('R',100, 100, GameConstants.ENEMY_WIDTH, GameConstants.ENEMY_HEIGHT, 100, new LinkedList<>(), 0, 0, 0, null, false);
         Random random = new Random();
         int randomNumber =random.nextInt(100);
         System.out.println(randomNumber);
         mockEnemy.shot(randomNumber);
         assertEquals(mockEnemy.getHpBar().getCurrentHealth(), mockEnemy.getEnemyHealth());
     }
+
+
 
     @AfterAll
     public static void tearDown() {
